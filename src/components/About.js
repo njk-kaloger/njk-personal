d1aefd2f15f5359ee226cd86e755ec36c1320031
Nicholas Kaloger <nicholaskaloger@Nicholass-MacBook-Pro.local>
2018-07-23 13:21:00 +1000

    Modified the about page and its styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import ImageCard from "./ImageCard";
 
 class About extends Component {
   render() {
@@ -8,6 +9,10 @@ class About extends Component {
       <div>
         <h1 id="title">About Page</h1>
         <Navigation />
+        <h2 id="subtitle">About Me</h2>
+        <div className="third-container">
+          <ImageCard name="Nicholas Kaloger" image="images/profile.jpg" />
+        </div>
         <Footer />
       </div>
     );
