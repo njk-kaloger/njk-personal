f8ff29ed7832e2be27727bd3b5d67077c8bc08a5
Nicholas Kaloger <nicholaskaloger@Nicholass-MacBook-Pro.local>
2018-07-24 17:25:45 +1000

    Small alterations

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,26 +15,28 @@ class Contact extends Component {
         <br />
         <br />
         <br />
-        <div className="flex-container">
-          <ContactCard
-            type="Personal"
-            email="njk.kaloger@gmail.com"
-            number="0422-127-623"
-          />
-          <ContactCard
-            type="Swinburne"
-            email="101622489@student.swin.edu.au"
-            number="0422-127-623"
-          />
-          <ContactCard
-            type="Inlight"
-            email="nicholas.kaloger@inlight.com"
-            number="0422-127-623"
-          />
-        </div>
+        <div className="main">
+          <div className="flex-container">
+            <ContactCard
+              type="Personal"
+              email="njk.kaloger@gmail.com"
+              number="0422-127-623"
+            />
+            <ContactCard
+              type="Swinburne"
+              email="101622489@student.swin.edu.au"
+              number="0422-127-623"
+            />
+            <ContactCard
+              type="Inlight"
+              email="nicholas.kaloger@inlight.com"
+              number="0422-127-623"
+            />
+          </div>
 
-        <div className="second-container">
-          <ImageCard name="Nicholas Kaloger" image="images/profile.jpg" />
+          <div className="second-container">
+            <ImageCard name="Nicholas Kaloger" image="images/profile.jpg" />
+          </div>
         </div>
         <Footer />
       </div>
